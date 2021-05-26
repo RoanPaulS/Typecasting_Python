d458009f52056b7b81f3548ac12420caa4783e16
Paul <paul@lintcloud.com>
2021-05-26 17:30:34 +0530

    Uploaded Typecasting of Python

---
+++ marks.py
@@ -0,0 +1,15 @@
+english = 98;
+tamil = 96.5;
+maths = 99;
+
+total = english+tamil+maths;
+print("Total mark is ",total);
+print(type(total));
+
+average = total/3;
+print("Average is ",average);
+print(type(average));
+
+print("Int of Average is ",int(average));
+
+print("Round of Average is ",round(average));
